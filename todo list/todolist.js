d0f5b10b52e89d7a4ac67469942cae4ffcd4353b
Arshad <arshad17006@gmail.com>
2025-12-08 16:01:19 +0530

    Version 1

---
+++ todo list/todolist.js
@@ -0,0 +1,45 @@
+const TodoList=[{name:'cooking',date:'12-03-2003'},{name:'washing',date:'12-05-2025'}];
+
+function finalListAdding(){
+    let finalresult='';
+    for( let i=0;i<TodoList.length ;i++){
+        const todoObject=TodoList[i];
+        const {name,date}=todoObject;
+        const html=`
+         <div>${name}</div> 
+        <div>${date}</div> 
+        <button onclick="TodoList.splice(${i},1) ; finalListAdding()" class="delete-button">Delete</button>`;
+        finalresult+=html;
+    }   
+    //console.log(finalresult);
+    document.querySelector('.ListValues').innerHTML=finalresult;
+}
+
+function handleEnter(event){
+    if(event.key== 'Enter'){
+        addOnClick();
+        
+    }
+}
+
+
+
+
+function addOnClick(){
+    const inputElem = document.querySelector('.js-addBox');
+    const inputName=inputElem.value;
+    const inputDateElem= document.querySelector('.inputDate');
+    const inputDate=inputDateElem.value;
+    TodoList.push({
+        name: inputName,
+        date:inputDate
+    });
+
+    console.log(TodoList);//
+    console.log(TodoList);//
+    inputElem.value='';
+    inputDateElem.value='';
+    finalListAdding();
+    
+}
+
